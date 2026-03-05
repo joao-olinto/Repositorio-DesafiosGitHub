1aa0f3fcaa1e5a986de5b6d3a76b1caa9b10ad18
Joao Paulo <joaoolintogomes159@gmail.com>
2026-03-05 20:20:04 -0300

    Adicionadas quantidades de vendas e media do ticket

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,9 +7,14 @@ public class Program {
 
 		System.out.println("Sistema  de sites de vendas");
 		double valorTotalVenda = 5000.00;
+		int quantidadeVendas = 200;
+		
+		
+		double ticketMedio = valorTotalVenda / quantidadeVendas;
 		
 		System.out.println();
 		System.out.println("Valor total de vendas: "+ String.format("%.2f",valorTotalVenda));
+		System.out.println("Ticket medio: "+ String.format("%.2f",ticketMedio));
 	}
 
 }
