4cd93af0a0ce32d4a2ae078647ad5f61961b72a3
Joao Paulo <joaoolintogomes159@gmail.com>
2026-03-05 20:09:02 -0300

    Adicionado o valor total de vendas

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,13 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+		System.out.println("Sistema  de sites de vendas");
+		double valorTotalVenda = 5000.00;
+		
+		System.out.println();
+		System.out.println("Valor total de vendas: "+ String.format("%.2f",valorTotalVenda));
 	}
 
 }
